e9cd129edb9e3d1e3066f8edce91bdca37ae6b0e
raphaelprados <74248473+raphaelprados@users.noreply.github.com>
2024-02-07 14:35:10 -0400

    adding benchmark run script

---
+++ run.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+if test -f ./logs/logis4.txt; then
+	rm ./logs/logis4.txt
+fi
+
+for i in {1..30}
+do
+	if test "$1" = "LU"; then
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4.2-MPI/bin/lu.C.x | tee -a LU
+	else if "$1" = "FT"; then
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4.2-MPI/bin/lu.C.x | tee -a LU		
+	else if "$1" = "BT"; then
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4.2-MPI/bin/lu.C.x | tee -a LU			
+	wait
+done
