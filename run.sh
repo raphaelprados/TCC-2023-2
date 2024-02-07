6f5531be846c737d9f5a82e17356123a9c228e99
raphaelprados <74248473+raphaelprados@users.noreply.github.com>
2024-02-07 14:35:37 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if test -f ./logs/logis4.txt; then
 	rm ./logs/logis4.txt
 fi
 
-for i in {1..30}
+for i in {1..16}
 do
 	if test "$1" = "LU"; then
 		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4.2-MPI/bin/lu.C.x | tee -a LU
