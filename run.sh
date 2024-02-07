2b86eb15baea3e37d876a193f545fece84d9573e
raphaelprados <74248473+raphaelprados@users.noreply.github.com>
2024-02-07 14:44:13 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,16 +1,14 @@
 #! /bin/sh
 
-if test -f ./logs/logis4.txt; then
-	rm ./logs/logis4.txt
-fi
-
 for i in {1..16}
 do
 	if test "$1" = "LU"; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4.2-MPI/bin/lu.C.x | tee -a LU
-	else if "$1" = "FT"; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4.2-MPI/bin/lu.C.x | tee -a LU		
-	else if "$1" = "BT"; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4.2-MPI/bin/lu.C.x | tee -a LU			
-	wait
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/lu.C.x | tee -a LU
+	elif "$1" = "FT"; then
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/ft.C.x | tee -a FT		
+	elif "$1" = "BT"; then
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/bt.C.x | tee -a BT			
+	else
+ 		echo "Condition missing: benchmarkd not identified (LU, BT, FT)
+ 	fi
 done
