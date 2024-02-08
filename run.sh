0c59e9b484a07db343a9af2c572b034a9dbce217
raphaelprados <74248473+raphaelprados@users.noreply.github.com>
2024-02-08 08:30:33 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,39 +1,15 @@
-#! /bin/bash
+#! /bin/sh
 
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <benchmark>"
-    exit 1
+if test -f ./logs/logis4.txt; then
+	rm ./logs/logis4.txt
 fi
 
-# Remove the log file if it exists
-if [ -f ./logs/logis4.txt ]; then
-    rm ./logs/logis4.txt
-fi
-
-# Define output file based on the benchmark
-output_file=""
-case "$1" in
-    LU)
-        output_file="LU_Output.txt"
-        ;;
-    BT)
-        output_file="BT_Output.txt"
-        ;;
-    FT)
-        output_file="FT_Output.txt"
-        ;;
-    *)
-        echo "Invalid benchmark. Supported benchmarks: LU, BT, FT"
-        exit 1
-        ;;
-esac
-
-# Execute the benchmark 30 times
-for ((i = 1; i <= 30; i++)); do
-    echo "Execution $i" >> ./logs/logis4.txt
-    SECONDS=0
-    dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/"$1".C.x | tee -a "$output_file"
-    time=$SECONDS
-    echo "Time: $time" >> ./logs/logis4.txt
+for i in {1..30}; do
+	echo execucao $i >> ./logs/logis4.txt
+	SECONDS=0
+	dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/lu.C.x | tee -a Lu_ouptut.txt
+	wait
+	time=$SECONDS
+	echo time: $time >> ./logs/logis4.txt
+	
 done
