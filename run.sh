42d63b7d9a6528f7c69db78fd2a2de3075ca182c
raphaelprados <74248473+raphaelprados@users.noreply.github.com>
2024-02-08 07:39:41 -0400

    Updating run script to address single execution issues

--- run.sh
+++ run.sh
@@ -1,14 +1,24 @@
 #! /bin/sh
 
-for i in {1..16}
+if test -f ./logs/logis4.txt; then
+	rm ./logs/logis4.txt
+fi
+
+for i in {1..30}
 do
-	if test "$1" = "LU"; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/lu.C.x | tee -a LU
-	elif "$1" = "FT"; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/ft.C.x | tee -a FT		
-	elif "$1" = "BT"; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/bt.C.x | tee -a BT			
-	else
- 		echo "Condition missing: benchmarkd not identified (LU, BT, FT)
- 	fi
+	echo execucao $i >> ./logs/logis4.txt
+	SECONDS=0
+	if "$1" == "LU"; then
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/lu.C.x | tee -a LU_Ouptut
+	elif "$1" == "BT"; then
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/bt.C.x | tee -a LU_Ouptut
+	elif "$1" == "FT"; then
+		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/ft.C.x | tee -a LU_Ouptut
+	else 
+		echo "No valid parameter was found! Can't run benchmark. You must tell which NPB benchmark you want to run (LU, BT, FT)"
+	fi
+	wait
+	time=$SECONDS
+	echo time: $time >> ./logs/logis4.txt
+	
 done
