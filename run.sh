7f0875920ad187555b6727cbed88607448af01c7
raphaelprados <74248473+raphaelprados@users.noreply.github.com>
2024-02-08 08:38:19 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,10 +4,11 @@ if test -f ./logs/logis4.txt; then
 	rm ./logs/logis4.txt
 fi
 
-for i in {1..30}; do
+for i in {1..30}
+do
 	echo execucao $i >> ./logs/logis4.txt
 	SECONDS=0
-	dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/lu.C.x | tee -a Lu_ouptut.txt
+	dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/lu.C.x | tee -a Lu_output.txt
 	wait
 	time=$SECONDS
 	echo time: $time >> ./logs/logis4.txt
