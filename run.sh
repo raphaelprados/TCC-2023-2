c1242f46993e011bfd05b64d0104cce9857a64c8
raphaelprados <74248473+raphaelprados@users.noreply.github.com>
2024-02-08 08:07:46 -0400

    Changed run.sh structure so that it can run properly

--- run.sh
+++ run.sh
@@ -4,21 +4,28 @@ if test -f ./logs/logis4.txt; then
 	rm ./logs/logis4.txt
 fi
 
-for i in {1..30}
-do
-	echo execucao $i >> ./logs/logis4.txt
-	SECONDS=0
-	if [ "$1" = "LU" ]; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/lu.C.x | tee -a LU_Ouptut
-	elif [ "$1" = "BT" ]; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/bt.C.x | tee -a LU_Ouptut
-	elif [ "$1" = "FT" ]; then
-		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/ft.C.x | tee -a LU_Ouptut
-	else 
+output_file=""
+
+case "$1" in
+	lu)
+		output_file = "Lu_output.txt"
+		;;
+	bt)
+		output_file = "Bt_output.txt"
+		;;
+	ft)
+		output_file = "Ft_output.txt"
+		;;
+	*)
 		echo "No valid parameter was found! Can't run benchmark. You must tell which NPB benchmark you want to run (LU, BT, FT)"
-	fi
-	wait
+		exit 1
+		;;
+esac
+
+for((i = 1; i <= 32; i++)); do
+	echo execucao $i >> ./logs/log"$1".txt
+	SECONDS=0
+	dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/"$1".C.x | tee -a "$output_file"
 	time=$SECONDS
-	echo time: $time >> ./logs/logis4.txt
-	
+	echo time: $time >> ./logs/log"$1".txt
 done
