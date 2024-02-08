cf6ee80c0d71a4b8ed5ff9a3fbd88328196ddd4a
raphaelprados <74248473+raphaelprados@users.noreply.github.com>
2024-02-08 07:55:43 -0400

    fixing comparison issue

--- run.sh
+++ run.sh
@@ -8,11 +8,11 @@ for i in {1..30}
 do
 	echo execucao $i >> ./logs/logis4.txt
 	SECONDS=0
-	if "$1" == "LU"; then
+	if [ "$1" = "LU" ]; then
 		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/lu.C.x | tee -a LU_Ouptut
-	elif "$1" == "BT"; then
+	elif [ "$1" = "BT" ]; then
 		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/bt.C.x | tee -a LU_Ouptut
-	elif "$1" == "FT"; then
+	elif [ "$1" = "FT" ]; then
 		dmtcp_launch --join-coordinator mpirun -np 4 ./NPB3.4.2/NPB3.4-MPI/bin/ft.C.x | tee -a LU_Ouptut
 	else 
 		echo "No valid parameter was found! Can't run benchmark. You must tell which NPB benchmark you want to run (LU, BT, FT)"
